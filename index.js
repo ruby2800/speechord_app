34f76023b5a478ce70e63c342d9f69b0453f988e
余函倩 <you@example.com>
2020-08-01 18:38:43 +0800

    歷史紀錄

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 import 'react-native-paper';
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './pages/history';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- pages/history.js
+++ pages/history.js
@@ -7,19 +7,6 @@ import { DrawerActions, useNavigation } from '@react-navigation/native';
 import { AudioUtils } from 'react-native-audio';
 import { ScrollView } from 'react-native-gesture-handler';
 
-let test = [];
-
-
-
-// //import WordFile from'./WordFile';
-// 
-
-
-//ReadDir();
-
-
-
-
 
 export default class history extends Component {
     state = {
@@ -27,14 +14,9 @@ export default class history extends Component {
     }
 
 
+
     componentDidMount() {
         this.ReadDir();
-        // this.state ={
-        //     list2 : 'ruvy'
-        // }
-
-
-        //this.readDir();
     }
 
     // 读取目录
@@ -46,102 +28,45 @@ export default class history extends Component {
                 var reg = new RegExp("^.*aac.*$");
 
                 let output = [];
-                let res = [];
 
 
                 if (result && result.length > 0) {
 
-                    let file_c = 0;
-
                     for (let i = 0; i < result.length; i++) {
 
-
                         if (reg.test(result[i].name)) {
-                         //   console.log(result[i].name);
-                            // console.log(result[i].path);
-                            // e.preventDefault();
-
-
+                            // console.log(result[i].name);
+                            // console.log(result[i].path);                  
                             let obj = { 'name': result[i].name, 'path': result[i].path };
 
                             output.push(obj);
-                            // console.log("push" + test);
-                            //console.log(file_c);
-                          //  console.log("名子" + output[file_c].name)
-                            file_c++;
-                            this.setState({
-                                response: output
-                            })
+                            // console.log(output[file_c].name);
 
                         }
-
                     }
-                    //我趴媽剩一點了幹就是這個啦幹
-                    //幹
-                    // let newArray = [...this.state.array];
-                    // this.setState({ list2: output })
+                    this.setState({
+                        response: output
+                    })
 
                 }
-
-
-                // console.log("CONFUSED "+ReadDir.name[3]);
-                // return (
-                //     output
-                // )
             })
 
             .catch((err) => {
                 console.log("錯誤" + err.message + err.code);
             });
-        // console.log("結果" + res.output);
 
-        return (
-            // <View>
-            //     {
-            //         output.map((l, i) => (
-            //             <ListItem
-            //                 key={i}
-            //                 // play-circle-filled
-            //                 leftIcon={{ name: 'mic' }}
-            //                 title={l.name}
-            //                 subtitle={l.subtitle}
-            //                 bottomDivider
-            //                 rightIcon={{ name: 'delete' }}
-            //                 onPress={() => navigation.navigate('播放', { url: l.path, time: 5 })}
-            //                 onLongPress={() => {
-
-            //                     Alert.alert(
-            //                         "提醒",
-            //                         "確定要刪除嗎",
-            //                         [
-            //                             {
-            //                                 text: "確定",
-            //                                 onPress: () => this.deleteFile("/data/user/0/com.helloworld2/files/'name'+1595830313260.aac"),
-            //                                 style: "cancel"
-            //                             },
-            //                             { text: "沒有", onPress: () => console.log("OK Pressed") }
-            //                         ],
-            //                         { cancelable: false }
-            //                     );
-            //                 }}
-            //             />
-            //             // this.state.currentTime
-            //         ))
-            //     }
-            // </View>
-
-            <Text>sdfsd</Text>
-        )
     }
 
 
     //删除文件
     async deleteFile(filePath) {
+
         const path = filePath;
         const res = await RNFS.unlink(path)
             .then(() => {
-                this.ReadDir();
+
                 console.log('FILE DELETED');
+                this.ReadDir();
             })
             .catch((err) => {
                 console.log(err.message);
@@ -150,45 +75,12 @@ export default class history extends Component {
     }
 
     render() {
-        //this.ReadDir();
-        response = this.state.response;
-        console.log("render" + response);
-
-
-        // 
-        //console.log(name);
-        // let time = this.state;
-        // const { navigation } = this.props;
-        // let { play, pause } = this.state;
-
-
-        // console.log(this.list2);
-
-        // let  {list2} = this.state;
-
-
-
-        const list = [
-            {
-                name: '2020-7-27-19-58.aac',
-                path: '/data/user/0/com.helloworld2/files/2020-7-27-21-0.aac'
-
-
-            },
-            {
-                name: 'Chris Jackson',
-                path: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-
-            },
-
-        ]
+        let { response } = this.state;
         const { navigation } = this.props;
 
         return (
 
             <View style={{ flex: 1 }} >
-
-
                 <Header
                     backgroundColor='transparent'
                     containerStyle={{ width: '100%', backgroundColor: '#3488C0', borderBottomWidth: 0 }}
@@ -210,8 +102,6 @@ export default class history extends Component {
                 />
 
                 <View>
-                    <Text>DFGDF</Text>
-                    {/* <ReadDir /> */}
                 </View>
                 <ScrollView>
                     <View>
